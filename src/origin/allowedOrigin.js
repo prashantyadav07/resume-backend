55cf387ff47ccc0c1f82f6f6f7b31303ad3eca79
prashant <prashantyadav9407@gmail.com>
2025-04-26 17:57:14 +0530

    farg

--- src/origin/allowedOrigin.js
+++ src/origin/allowedOrigin.js
@@ -1,7 +1,7 @@
 export const allowedOrigins = [
     "http://localhost:5173",
-    "https://resume-backend-nine.vercel.app/",
-    "https://resume-backend-nine.vercel.app",
+    "https://miniresumeanalyzer.netlify.app/",
+    "https://miniresumeanalyzer.netlify.app",
     "https://resume-backend-git-main-prashant-yadavs-projects-1570311f.vercel.app/",
     "https://resume-backend-git-main-prashant-yadavs-projects-1570311f.vercel.app",
     "https://resume-backend-3yu3v59ut-prashant-yadavs-projects-1570311f.vercel.app/",
